28c644929bfb8385749e6b120be88bb1b02ac8fa
Mandy1007 <kitty911007@gmail.com>
2015-11-11 00:03:02 -0600

    update

---
+++ README.rst
@@ -0,0 +1,4 @@
+pyskel
+======
+
+Try clone
---
+++ setup.py
@@ -0,0 +1,33 @@
+from codecs import open as codecs_open
+from setuptools import setup, find_packages
+
+
+# Get the long description from the relevant file
+with codecs_open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='lightening_alert',
+      version='0.0.1',
+      description=u"Area Lightning Alert",
+      long_description=long_description,
+      classifiers=[],
+      keywords='',
+      author=u"Mandy Wu",
+      author_email='kitty911007@gmail.com',
+      url='https://github.com/Mandy1007/lightning_alert.git',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'click'
+      ],
+      extras_require={
+          'test': ['pytest'],
+      },
+      entry_points="""
+      [console_scripts]
+      lightning_alert=lightning_alert.scripts.cli:cli
+      """
+      )
